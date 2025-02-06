56884baa65a103dec263d916bc6872a2efb380fd
salina kiran <salinakiran225@gmail.com>
2025-02-06 06:34:54 +0530

    Reverting the file " updated order.txt to virat"
    This reverts commit cfae9dbfe6e52675dfaf404032d93c183c5a4d28.

--- order.java
+++ order.java
@@ -2,4 +2,6 @@
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello World"); //Prints on screen “ virat "
+		System.out.println("Hello World"); //Prints on screen “Hello World”
+						   //}
+						   //
