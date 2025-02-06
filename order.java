cfae9dbfe6e52675dfaf404032d93c183c5a4d28
salina kiran <salinakiran225@gmail.com>
2025-02-06 06:28:53 +0530

    updated order.txt to virat

--- order.java
+++ order.java
@@ -2,6 +2,4 @@
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello World"); //Prints on screen “Hello World”
-						   //}
-						   //
+		System.out.println("Hello World"); //Prints on screen “ virat "
